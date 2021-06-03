61154ae0c00d83817dafded55d9fe8ba6e50fcfa
Jesse Wenger <wengerj@gmail.com>
2021-06-03 22:47:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+  books = fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
     .then(json => renderBooks(json));
+  return books;
 }
 
 function renderBooks(books) {
